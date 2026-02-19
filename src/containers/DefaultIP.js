3ba1a8408ee0c6e130365347815e5138259af443
krisnadewangga <krisnadewanggapp@gmail.com>
2026-02-19 14:12:26 +0700

    coba push

--- src/containers/DefaultIP.js
+++ src/containers/DefaultIP.js
@@ -3,7 +3,7 @@
 //export default 'http://127.0.0.1/agogo-backend/public';
 // export default 'http://192.168.1.30/agogo';
 
-// export default 'https://pos.agogo-bakery.com';
-export default 'http://127.0.0.1:8000';
+export default 'https://pos.agogo-bakery.com';
+// export default 'http://127.0.0.1:8000';
 //export default 'http://192.168.1.30/agogo';
 
