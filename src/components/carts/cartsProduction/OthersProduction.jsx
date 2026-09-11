b63687731d40683f7720f63c160d41a591335a93
Krisna Dewangga <krisna.dewangga@kemenkeu.go.id>
2026-09-11 15:57:08 +0700

    update label produksi

--- src/components/carts/cartsProduction/OthersProduction.jsx
+++ src/components/carts/cartsProduction/OthersProduction.jsx
@@ -21,12 +21,12 @@ class OthersProduction extends React.Component {
                     <th><i className="fas fa-minus-circle add-product"> Lain-lain</i></th>
                 </tr>
                 <tr>
-                    <td className="production">Rusak</td>
+                    <td className="production">Rusak/Lain-lain</td>
                     <td className="product-total-broken text-right" >{ this.props.cartStore.state.produksi[this.props.cartStore.state.selectedProduct.name + "rusak"] || "-"}</td>
                     <td><button type="button" className={this.props.cartStore.productionButton4()} onClick={() => this.props.modalStore.toggleModal('productionOther', 'lg', "4")}><i className="fas fa-pen-square edit"></i></button></td>
                 </tr>
                 <tr>
-                    <td className="production">Lain-lain</td>
+                    <td className="production">Airmadidi</td>
                     <td className="product-total-others text-right" >{ this.props.cartStore.state.produksi[this.props.cartStore.state.selectedProduct.name + "lain"] || "-"}</td>
                     <td><button type="button" className={this.props.cartStore.productionButton5()} onClick={() => this.props.modalStore.toggleModal('productionOther', 'lg', "5")}><i className="fas fa-pen-square edit"></i></button></td>
                 </tr>
